51b44a6146b8074e968910144df3ccc0f2464782
bert386 <eastecdev2011@gmail.com>
2025-08-01 10:08:37 +1000

    clean

--- api/get-ebay-data.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
-  // Placeholder: handle eBay API lookups
-  res.status(200).json({ message: 'eBay data fetched.' });
-}
