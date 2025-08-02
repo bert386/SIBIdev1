16aa3f16939570f88fa69df0262dd62824f58516
bert386 <eastecdev2011@gmail.com>
2025-08-02 11:44:10 +1000

    Update analyse-image.js

--- api/analyse-image.js
+++ api/analyse-image.js
@@ -47,4 +47,3 @@ module.exports.default = async function handler(req, res) {
       res.status(200).json({ items: results.flat() });
       res.status(500).json({ error: "Failed to analyse images." });
     }
-};
